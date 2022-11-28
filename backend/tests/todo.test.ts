82906cd121809171c250c37d915020bd8b61d98b
Sophearah <sophearah.suy-puth@mail.mcgill.ca>
2022-11-28 16:25:14 -0500

    added test for adding todo item route

--- backend/tests/todo.test.ts
+++ backend/tests/todo.test.ts
@@ -26,11 +26,23 @@ describe('Todo tests', () => {
         const newTodo = new Todo()
         newTodo.taskName = "COMP 251 A1";
         const newTodo2 = new Todo()
-        newTodo2.taskName = "PSYC 538 10a";
+        newTodo2.taskName = "MATH 223 A1";
         await TodoRepository.save(newTodo);
         await TodoRepository.save(newTodo2);
         const res = await request.get("/api/todos").send();
         expect(res.statusCode).toBe(200);
         expect(res.body.todos.length).toBe(2);
+    });
+
+    it('should add todo item', async () => {
+        const newTodoItem = {
+            completed: false,
+            taskName: "do laundry"
+        };
+
+        const res = await request.post("/api/todos").send(newTodoItem);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.taskName).toBe("do laundry");
     });
 });
